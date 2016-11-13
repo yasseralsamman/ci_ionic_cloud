e8b6dc4027ef1ddc74d6fab0b212e729d2b57a67
Mohammad Yasser AlSamman <mys.zanarkand.89@gmail.com>
2016-11-13 13:46:18 +0200

    clean version

--- application/config/ci_ionic_cloud.php
+++ application/config/ci_ionic_cloud.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['cloud_api_token'] =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxZTM0ODZjZS05MDRjLTQxYjAtOWZmNS04MTZlODViZmI3M2MifQ.AG7IK_PAfNceSn_NLXRuy6tO5m_itqlWRzmFvr2R__g';
-$config['cloud_app_id']    =  'ce7f2362' ;
+$config['cloud_api_token'] =  '';
+$config['cloud_app_id']    =  '' ;
